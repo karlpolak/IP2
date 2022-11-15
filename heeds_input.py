81400f1481efb42c72489279d9b37250ce97a72f
Anton <antonhens@live.be>
2022-11-15 12:45:37 +0100

    WIP Heeds scripts

--- heeds_input.py
+++ heeds_input.py
@@ -1,21 +1,66 @@
 from pickle import load 
+import numpy as np
+import geopandas as gpd
+import pandas as pd
 import os
 from dyntapy.visualization import show_network
+from dyntapy.demand_data import od_graph_from_matrix
+from pyproj import Proj, transform
+from dyntapy.assignments import StaticAssignment
+import warnings
+warnings.filterwarnings('ignore') # hide warnings
 
+# INPUT FROM HEEDS 
+toll_value = 0
+
+# Link we decided to toll
+toll_id = 1490
 
 # FILL THIS IN ACCORDING TO YOUR NEEDS
-city = 'Brussels'
-buffer_N = 1
-buffer_transit = "45"
-buffer_N_string = str(buffer_N)
+city = 'BRUSSEL'
+buffer_N = 3
+# buffer_transit = "45"
 
-
-
+# SPECIFY THE PATHS 
+buffer = str(buffer_N)
 HERE = os.path.dirname(os.path.realpath("__file__"))
-network_path = HERE + os.path.sep +'STA_prep' + os.path.sep + 'network_data' + os.path.sep + 'BRUSSEL_' + buffer_N_string + '_centroids'
+data_path = HERE + os.path.sep +'STA_prep'
+zoning_path = data_path + os.path.sep + "shapefile_data" + os.path.sep + city + "_" + buffer+ "_10" + os.path.sep + city + "_" + buffer+ "_10.shp"
+network_path = data_path + os.path.sep + 'network_data' + os.path.sep + city + "_" + buffer + '_centroids'
+od_path = data_path + os.path.sep + "od_matrix_data_ext_tr" + os.path.sep + city + "_ext_tr_" + buffer + "_9_10.xlsx"
 
+# LOAD NETWORK FILE
 with open(network_path, 'rb') as network_file:
     g = load(network_file)
-    print(f'network saved at f{network_path}')
-
+    print(f'network loaded from f{network_path}')
 show_network(g)
+
+# LOAD OD-MATRIX
+od = pd.read_excel(od_path)
+od_array = od.to_numpy() # The OD matrix is now stored in a numpy array
+
+# LOAD THE CENTROIDS
+zoning = gpd.read_file(zoning_path) 
+x_lamb, y_lamb = zoning["X_LAMB"], zoning["Y_LAMB"]
+x_lamb, y_lamb = x_lamb.to_numpy(), y_lamb.to_numpy()
+inProj, outProj = Proj(init='epsg:31370'), Proj(init='epsg:4326')
+x_centroids, y_centroids = transform(inProj,outProj,x_lamb,y_lamb)
+
+# CREATE OD-GRAPH
+od_graph = od_graph_from_matrix(od_array,x_centroids,y_centroids)
+
+# DO ASSIGNMENT 
+tolls = np.zeros(g.number_of_edges())
+tolls[toll_id] = toll_value
+assignment = StaticAssignment(g,od_graph, tolls)
+methods = ['dial_b']
+for method in methods:
+    result = assignment.run(method)
+    show_network(g, flows = result.flows, notebook=True, show_nodes=False)
+    print(f'{method=} ran successfully')
+print(result.__dict__.keys())
+
+# OBJECTIVE VALUE = OUTPUT
+
+objective_value = result.link_costs * result.link_flows
+
--- heeds_prep.py
+++ heeds_prep.py
@@ -10,27 +10,26 @@ from pickle import dump
 warnings.filterwarnings('ignore') # hide warnings
 
 # FILL IN THOSE VARIABLES ACCORDING TO YOUR NEEDS
-city = 'Brussels'
-buffer_N = 1
-buffer_transit = "45"  # Only used in the name of the file we save the network to. 
-
+city = 'BRUSSEL'
+buffer_N = 3
+#buffer_transit = "45"  # Only used in the name of the file we save the network to. 
 
 # Create network file
-buffer_N_string = str(buffer_N) 
+buffer = str(buffer_N) 
 buffer_N = buffer_N * 1000
 g = road_network_from_place(city, buffer_dist_close=buffer_N)
+g = relabel_graph(g)
+show_network(g)
 
 # Add the centroids to the network, which can be derived from the zoning file. 
 HERE = os.path.dirname(os.path.realpath("__file__"))
-zoning_path = HERE + os.path.sep +'STA_prep' + os.path.sep + 'shapefile_data' + os.path.sep + 'BRUSSEL_' + buffer_N_string + '_10' + os.path.sep + 'BRUSSEL_' + buffer_N_string + '_10.shp'
-network_path = HERE + os.path.sep +'STA_prep' + os.path.sep + 'network_data' + os.path.sep + 'BRUSSEL_' + buffer_N_string + '_centroids'
+data_path = HERE + os.path.sep +'STA_prep'
+zoning_path = data_path + os.path.sep + 'shapefile_data' + os.path.sep + city + "_" + buffer + '_10' + os.path.sep + city + "_" + buffer + '_10.shp'
+network_path = data_path + os.path.sep + 'network_data' + os.path.sep + city + "_" + buffer + '_centroids'
 zoning = gpd.read_file(zoning_path) 
-x_lamb = zoning["X_LAMB"]
-x_lamb = x_lamb.to_numpy()
-y_lamb = zoning["Y_LAMB"]
-y_lamb = y_lamb.to_numpy()
-inProj = Proj(init='epsg:31370')
-outProj = Proj(init='epsg:4326')
+x_lamb, y_lamb = zoning["X_LAMB"], zoning["Y_LAMB"]
+x_lamb, y_lamb = x_lamb.to_numpy(), y_lamb.to_numpy()
+inProj, outProj = Proj(init='epsg:31370'), Proj(init='epsg:4326')
 x_centroids, y_centroids = transform(inProj,outProj,x_lamb,y_lamb)
 g = add_centroids(g, x_centroids,y_centroids,k=1, method='link')
 g = relabel_graph(g)
