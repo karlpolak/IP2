1e2c6dc3e2c68c1b68b9deadd3a07c782449c960
Anton <antonhens@live.be>
2023-05-03 22:03:19 +0200

    WIP clear overview

--- toll_optimization/case_studies/toy_network_inelastic_demand/Inelastic_toy_HEEDS_input.py
+++ toll_optimization/case_studies/toy_network_inelastic_demand/Inelastic_toy_HEEDS_input.py
@@ -3,12 +3,15 @@ warnings.filterwarnings('ignore') # hide warnings
 import numpy as np
 from pickle import load
 from dyntapy.assignments import StaticAssignment
+from dyntapy.toll import create_toll_object
 
-# INPUT FROM HEEDS: TOLL VALUE?  
+# INPUT FROM HEEDS = toll value(s)
 toll_value = 0
-# LINK(S) THAT WE WOULD LIKE TO TOLL
+# OUR INPUT: toll scheme and links to toll
 toll_ids = [2]
+toll_method = 'single'
 
+# Retrieve network and od-graph
 network_path = "C:/Users/anton/IP2/HEEDS_prep/network_centroids_data/two_route"
 graph_path = "C:/Users/anton/IP2/HEEDS_prep/od_graph_data/two_route"
 with open(network_path, 'rb') as network_file:
@@ -18,12 +21,11 @@ with open(graph_path, 'rb') as f:
     od_graph = load(f)
     print(f'od_graph saved at f{graph_path}')
 
-
-tolls = np.zeros(g.number_of_edges())
-for elem in toll_ids:
-    tolls[elem] = toll_value
-assignment = StaticAssignment(g, od_graph, tolls) # TODO fix tolls
+# Create toll object
+toll_object = create_toll_object(g, toll_method, toll_ids, toll_value)
+assignment = StaticAssignment(g, od_graph, toll_object)
 result = assignment.run('dial_b')
 
+# Output for HEEDS
 veh_hour_per_link = result.link_costs * result.flows
 objective = sum(veh_hour_per_link)
